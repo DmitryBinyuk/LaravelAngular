5dfb87644277fc7d231279e51602bc73eee5d022
Dmitry Binyuk <dmitry.binyuk@chisw.com>
2017-04-26 02:14:47 +0300

    admin2

---
+++ resources/views/admin/dashboard/index.blade.php
@@ -0,0 +1,9 @@
+@extends('admin.layout.base')
+
+@section('page-content')
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">Übersicht</h1>
+        </div>
+    </div>
+@endsection
